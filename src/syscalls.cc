195e2e80a515ae8fb74b4c5fff158dca1b0ff9c1
macournoyer <macournoyer@gmail.com>
2012-09-04 20:40:03 -0400

    Bring args closer to real syscalls.

--- src/syscalls.cc
+++ src/syscalls.cc
@@ -15,21 +15,32 @@ using namespace v8;
     ThrowException(Exception::Error(String::New(strerror(errno))))
 
 Handle<Value> Socket(const Arguments& args) {
-  HandleScope scope;
+  if (args.Length() != 3) {
+    return TYPE_ERROR("Wrong number of arguments. Expecting domain, type, protocol.");
+  }
+
+  if (!args[0]->IsNumber() || !args[1]->IsNumber() || !args[2]->IsNumber()) {
+    return TYPE_ERROR("Wrong type of arguments. Expecting numbers");
+  }
   
-  int fd = socket(AF_INET, SOCK_STREAM, 0);
+  int domain = args[0]->NumberValue();
+  int type = args[1]->NumberValue();
+  int protocol = args[2]->NumberValue();
+  
+  int fd = socket(domain, type, protocol);
   if (fd < 0) return SYS_ERROR();
   
   int sock_flags = 1;
   int ret = setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &sock_flags, sizeof(sock_flags));
   if (ret < 0) return SYS_ERROR();
   
+  HandleScope scope;
   return scope.Close(Number::New(fd));
 }
 
 Handle<Value> Fcntl(const Arguments& args) {
   if (args.Length() != 3) {
-    return TYPE_ERROR("Wrong number of arguments");
+    return TYPE_ERROR("Wrong number of arguments. Expecting FD, command, value.");
   }
 
   if (!args[0]->IsNumber() || !args[1]->IsNumber() || !args[2]->IsNumber()) {
@@ -48,7 +59,7 @@ Handle<Value> Fcntl(const Arguments& args) {
 
 Handle<Value> Connect(const Arguments& args) {
   if (args.Length() != 3) {
-    return TYPE_ERROR("Wrong number of arguments");
+    return TYPE_ERROR("Wrong number of arguments. Expecting FD, port, address.");
   }
 
   if (!args[0]->IsNumber() || !args[1]->IsNumber() || !args[2]->IsString()) {
@@ -72,7 +83,7 @@ Handle<Value> Connect(const Arguments& args) {
 
 Handle<Value> Bind(const Arguments& args) {
   if (args.Length() != 3) {
-    return TYPE_ERROR("Wrong number of arguments");
+    return TYPE_ERROR("Wrong number of arguments. Expecting FD, port, address.");
   }
 
   if (!args[0]->IsNumber() || !args[1]->IsNumber() || !args[2]->IsString()) {
@@ -95,17 +106,18 @@ Handle<Value> Bind(const Arguments& args) {
 }
 
 Handle<Value> Listen(const Arguments& args) {
-  if (args.Length() != 1) {
-    return TYPE_ERROR("Wrong number of arguments");
+  if (args.Length() != 2) {
+    return TYPE_ERROR("Wrong number of arguments. Expecting FD, backlog");
   }
 
-  if (!args[0]->IsNumber()) {
-    return TYPE_ERROR("Wrong type of argument. Expecting number");
+  if (!args[0]->IsNumber() || !args[1]->IsNumber()) {
+    return TYPE_ERROR("Wrong type of argument. Expecting number, number");
   }
   
   int fd = args[0]->NumberValue();
+  int backlog = args[1]->NumberValue();
   
-  int ret = listen(fd, 511);
+  int ret = listen(fd, backlog);
   if (ret < 0) return SYS_ERROR();
   
   return Undefined();
@@ -113,7 +125,7 @@ Handle<Value> Listen(const Arguments& args) {
 
 Handle<Value> Accept(const Arguments& args) {
   if (args.Length() != 1) {
-    return TYPE_ERROR("Wrong number of arguments");
+    return TYPE_ERROR("Wrong number of arguments. Expecting FD.");
   }
 
   if (!args[0]->IsNumber()) {
@@ -132,20 +144,20 @@ Handle<Value> Accept(const Arguments& args) {
 }
 
 Handle<Value> Select(const Arguments& args) {
-  if (args.Length() != 2) {
-    return TYPE_ERROR("Wrong number of arguments");
+  if (args.Length() != 4) {
+    return TYPE_ERROR("Wrong number of arguments. Expecting [readables], [writable], [exceptionals], timeout.");
   }
 
-  if (!args[0]->IsArray() || !args[1]->IsArray()) {
-    return TYPE_ERROR("Wrong type of arguments. Expecting arrays");
+  if (!args[0]->IsArray() || !args[1]->IsArray() || !args[2]->IsArray() || !args[3]->IsNumber()) {
+    return TYPE_ERROR("Wrong type of arguments. Expecting array, array, array, number");
   }
   
   int nfds = 0; // Max fd+1 to watch
-  fd_set fds[2];
+  fd_set fds[3];
   
-  // Convert args to fd_sets. fds[0] = read fds, fds[1] = write fds.
+  // Convert args to fd_sets. fds[0] = read fds, fds[1] = write fds, fds[2] = error fds.
   unsigned set_i, i;
-  for(set_i = 0; set_i < 2; ++set_i) {
+  for(set_i = 0; set_i < 3; ++set_i) {
     FD_ZERO(&fds[set_i]);
     Handle<Array> array = Handle<Array>::Cast(args[set_i]);
     for(i = 0; i < array->Length(); ++i) {
@@ -155,11 +167,18 @@ Handle<Value> Select(const Arguments& args) {
     }
   }
   
-  int ret = select(nfds, &fds[0], &fds[1], 0, 0);
+  // Set the timeout (in sec) unless == 0
+  struct timeval timeout, *timeoutp = 0;
+  timeout.tv_usec = 0;
+  if ((timeout.tv_sec = args[3]->NumberValue()) > 0) timeoutp = &timeout;
+  
+  // Make the call!
+  int ret = select(nfds, &fds[0], &fds[1], &fds[2], timeoutp);
   if (ret < 0) return SYS_ERROR();
   
-  Local<Array> retfds[2];
-  for(set_i = 0; set_i < 2; ++set_i) {
+  // Convert the modified sets to arrays.
+  Local<Array> retfds[3];
+  for(set_i = 0; set_i < 3; ++set_i) {
     unsigned ret_i = 0;
     retfds[set_i] = Array::New();
     Handle<Array> array = Handle<Array>::Cast(args[set_i]);
@@ -172,15 +191,16 @@ Handle<Value> Select(const Arguments& args) {
   }
   
   HandleScope scope;
-  Local<Array> retarray = Array::New(2);
+  Local<Array> retarray = Array::New(3);
   retarray->Set(0, retfds[0]);
   retarray->Set(1, retfds[1]);
+  retarray->Set(2, retfds[2]);
   return scope.Close(retarray);
 }
 
 Handle<Value> Close(const Arguments& args) {
   if (args.Length() != 1) {
-    return TYPE_ERROR("Wrong number of arguments");
+    return TYPE_ERROR("Wrong number of arguments. Expecting FD.");
   }
 
   if (!args[0]->IsNumber()) {
@@ -197,11 +217,11 @@ Handle<Value> Close(const Arguments& args) {
 
 Handle<Value> Read(const Arguments& args) {
   if (args.Length() != 2) {
-    return TYPE_ERROR("Wrong number of arguments");
+    return TYPE_ERROR("Wrong number of arguments. Expecting FD, number of bytes.");
   }
 
   if (!args[0]->IsNumber() || !args[0]->IsNumber()) {
-    return TYPE_ERROR("Wrong type of argument. Expecting number");
+    return TYPE_ERROR("Wrong type of argument. Expecting number, number.");
   }
   
   int fd = args[0]->NumberValue();
@@ -223,11 +243,11 @@ Handle<Value> Read(const Arguments& args) {
 
 Handle<Value> Write(const Arguments& args) {
   if (args.Length() != 2) {
-    return TYPE_ERROR("Wrong number of arguments");
+    return TYPE_ERROR("Wrong number of arguments. Expecting FD, data.");
   }
 
   if (!args[0]->IsNumber() || !args[1]->IsString()) {
-    return TYPE_ERROR("Wrong type of argument. Expecting number & string");
+    return TYPE_ERROR("Wrong type of argument. Expecting number, string.");
   }
   
   int fd = args[0]->NumberValue();
@@ -270,6 +290,13 @@ void init(Handle<Object> target) {
   target->Set(String::NewSymbol("getpid"), FunctionTemplate::New(Getpid)->GetFunction());
   
   // Constants
+  // socket(2) options
+  target->Set(String::NewSymbol("AF_INET"), Number::New(AF_INET));
+  target->Set(String::NewSymbol("AF_UNIX"), Number::New(AF_UNIX));
+  target->Set(String::NewSymbol("AF_INET6"), Number::New(AF_INET6));
+  target->Set(String::NewSymbol("SOCK_STREAM"), Number::New(SOCK_STREAM));
+  target->Set(String::NewSymbol("SOCK_DGRAM"), Number::New(SOCK_DGRAM));
+  // fcntl(2) options
   target->Set(String::NewSymbol("F_SETFL"), Number::New(F_SETFL));
   target->Set(String::NewSymbol("O_NONBLOCK"), Number::New(O_NONBLOCK));
 }
